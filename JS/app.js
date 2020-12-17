eb4c5167cda36d3b0a524ed92290cae9986ca7c6
Luca <lucakanyo@protonmail.com>
2020-12-16 22:27:33 -0500

    complement function is working

--- JS/app.js
+++ JS/app.js
@@ -5,9 +5,32 @@ let revcompDiv = () => document.querySelector("#reverse-complement")
 let sequence = []
 const handleChange = (event) => {
     event.preventDefault();
-    sequence = event.target.value;
+    sequence = event.target.value.toUpperCase();
     
-    complementDiv().textContent = sequence
+    complementDiv().textContent = complement(sequence)
     reverseDiv().textContent = sequence
     revcompDiv().textContent = sequence
+
+    //complement(sequence)
+}
+
+function complement(sequence) {
+    let reversed = []
+    for (letter of sequence){
+        switch(letter){
+            case 'A': 
+                reversed.push('T')
+                break;
+            case 'T': 
+                reversed.push('A')
+                break;
+            case 'C': 
+                reversed.push('G')
+                break;
+            case 'G': 
+                reversed.push('C') 
+                break;
+        }
+    }
+    return reversed.join('')
 }
