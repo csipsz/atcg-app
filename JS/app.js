2f95f4b939cb633d71113bb861eb720c7c2f957d
Luca <lucakanyo@protonmail.com>
2020-12-16 21:23:26 -0500

    styling boxes

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,9 @@
-let user = {}
+
+let sequence = {}
 const handleChange = (event) => {
     event.preventDefault();
     let key = event.target.id;
     let value = event.target.value;
-    user[key] = value;
-    
-    console.log(user);
+    sequence[key] = value;
+    console.log(sequence[key]);
 }
