9d59fc614b74425b95db6cbea55f85d45efee78d
Luca <lucakanyo@protonmail.com>
2020-12-17 14:26:30 -0500

    added error check

--- JS/app.js
+++ JS/app.js
@@ -9,6 +9,8 @@ const handleChange = (event) => {
     //event.preventDefault();
     sequence = event.target.value.toUpperCase();
     event.target.value = event.target.value.toUpperCase();
+
+    handleError(event)
     complementBox().textContent = complement(sequence)
     reverseBox().textContent = reversing(sequence)
     revCompBox().textContent = complementReverse(sequence)  
@@ -32,6 +34,16 @@ function copyToClipBoard(element){
     document.execCommand("Copy")
 }
 
+
+function handleError(event){
+    let seq = event.target.value.split('')
+    for (char of seq){
+        if (char != 'A' &&  char != 'T' &&  char != 'C' &&  char != 'G') {
+            event.target.style.color = 'red'
+        }
+    }
+    setTimeout(function(){event.target.style.color = 'black'}, 5000)
+}
 
 
 
